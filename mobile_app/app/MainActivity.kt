930dcb7e6eb0c111bc0b53bbc252eade7f599148
alex03ac <alexandro.achalma.g@uni.pe>
2025-10-20 23:24:03 -0500

    Pantalla principal (interfaz y permisos)

---
+++ mobile_app/app/MainActivity.kt
@@ -0,0 +1,7 @@
+package com.af.shieldsms
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) { super.onCreate(savedInstanceState) }
+}
